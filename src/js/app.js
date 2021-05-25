f11dbf28009f70ef4af7db5faa750021ba2a9aae
arcodev <chris_arco0794@hotmail.com>
2021-05-25 12:44:15 -0500

    Agregando estilos a los servicios

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     iniciarApp();
 });
 
@@ -10,37 +10,64 @@ async function mostrarServicios() {
         //Utilizar fecth API para extraer informacion de los servicios del json
         const resultado = await fetch('./servicios.json');
         const db = await resultado.json();
-        const { servicios } = db;
+        const {
+            servicios
+        } = db;
 
         //Generar el HTML 
-        servicios.forEach( servicios => {
-            const { id, nombre, precio } = servicios;
-            
+        servicios.forEach(servicios => {
+            const {
+                id,
+                nombre,
+                precio
+            } = servicios;
+
             //DOM scripting
-            
+
             //Nombre del servicio
             const nombreServicio = document.createElement('P');
             nombreServicio.textContent = nombre;
             nombreServicio.classList.add('nombre-servicio');
-            
+
             //Precio del servicio 
             const precioServicio = document.createElement('P');
             precioServicio.textContent = `$ ${precio}`; //Template string
             precioServicio.classList.add('precio-servicio');
-            
+
             //Generar div contenedor del servicio
             const servicioDIV = document.createElement('DIV');
             servicioDIV.classList.add('servicio');
+            servicioDIV.dataset.idServicio = id;
             
+            
+            //Seleccionar un servicio para la cita y marcarlo con event handler
+            servicioDIV.onclick = seleccionarServicio;
+
             //inyectar nombre y precio al div
             servicioDIV.appendChild(nombreServicio);
             servicioDIV.appendChild(precioServicio);
-            
+
             //Inyectar en el HTML los servicios 
             document.querySelector('#servicios').appendChild(servicioDIV);
-            console.log(servicioDIV);
+            
         });
     } catch (error) {
         console.log(error);
     }
+}
+function seleccionarServicio(e) {
+    let elemento;
+    //Forzar que el elemento al cual le damos click sea el div
+    if(e.target.tagName === 'P') {
+        elemento = e.target.parentElement;
+    } else {
+        elemento = e.target;
+    }
+    //console.log(elemento.dataset.idServicio);
+    if(elemento.classList.contains('seleccionado')) {
+        elemento.classList.remove('seleccionado');
+    } else {
+        elemento.classList.add('seleccionado');
+    }
+    //elemento.classList.toggle('seleccionado');
 }
